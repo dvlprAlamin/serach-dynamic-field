f9a3240878e9a38fddee61f30da6a0aefffdf4fe
dvlpralamin <alaminhowladerofficial@gmail.com>
2022-11-30 00:21:41 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import { Fragment, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [fields, setFields] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v2/all").then(res => res.json()).then(data => {
+      setCountries(data)
+    })
+  }, [])
+
+  const addFieldHandler = (e) => {
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      const newField = <Fragment>
+        <input list={"country-list" + fields.length + 1} defaultValue={e.target.value} />
+        <datalist id={"country-list" + fields.length + 1}>
+          {
+            countries?.map(({ name }) => <option key={name} value={name} />)
+          }
+        </datalist>
+        <br /> <br />
+      </Fragment>
+      e.target.value = ""
+      setFields(pre => [...pre, newField])
+    }
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Choose country: <br /> <br /></label>
+      {
+        fields.length > 0 && fields.map((field, i) => <Fragment key={i}>{field}</Fragment>)
+      }
+
+      <input onKeyDown={addFieldHandler} list={"country-list"} />
+      <datalist id={"country-list"}>
+        {
+          countries?.map(({ name }) => <option key={name} value={name} />)
+        }
+      </datalist>
+
     </div>
   );
 }
